f863f2a675c1805079858711dff8537053f43225
VishDroid-dev <vk2345732@gmail.com>
2023-05-25 17:17:05 +0530

    updated cors

--- ctfplatform/settings.py
+++ ctfplatform/settings.py
@@ -32,6 +32,13 @@ CSRF_TRUSTED_ORIGINS = ["https://*.initcrew.com"]
 AUTH_USER_MODEL = "core.CustomUser"
 # Application definition
 
+CORS_ALLOWED_ORIGINS = [
+"https://ctf.initcrew.com",
+"https://*.initcrew.com",
+"http://localhost:8000",
+"http://127.0.0.1:8000"
+]
+
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/challenges/"
 
@@ -43,11 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
